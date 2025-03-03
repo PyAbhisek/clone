de9ee978978561fee0251c26237db7c71094029a
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-03 19:04:38 +0530

    removed the useEffect code

--- src/App.js
+++ src/App.js
@@ -5,29 +5,11 @@ import {useEffect} from "react"
 import Test from './component/test';
 
 function App() {
-    useEffect(() => {
-    
-      
-    }, [])
-    useEffect(() => {
-    
-      
-    }, [dependcy,first])
-    
-    useEffect(() => {
-    
-
-      return ()=>{
-        
-      }
-      
-    }, [dependcy,first])
-
   return (
     <div className="App">
 
-       {/* <Test/>
-        */}
+       <Test/>
+       
     </div>
   );
 }
